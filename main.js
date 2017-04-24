333eafbded1be347f86ebd31a2703840f0e6d92d
Dylan Foster <fosterdill@gmail.com>
2017-04-24 00:25:20 -0700

    2-3-1 nn working w gradient descent

--- main.js
+++ main.js
@@ -1,2 +1,108 @@
 #!/usr/bin/env node
-console.log('hi');
+const math = require('mathjs');
+
+let theta1 = math.random([4, 3], 0, 1);
+let theta2 = math.random([1, 4], 0, 1);
+let trainingCount = 1000;
+
+const alpha = 10;
+
+const sigmoid = value => {
+  return (1 / (1 + math.exp(-value)));
+};
+
+const matrixSigmoid = matrix => {
+  return matrix.map(value => {
+    return value.map(value => {
+      return sigmoid(value);
+    });
+  });
+};
+
+const forwardPropagate = (X, theta1, theta2) => {
+  let a1 = math.subset(X, math.index(math.range(0, 3), 0));
+  let a2 = matrixSigmoid(math.multiply(theta1, a1));
+  let a3 = sigmoid(math.squeeze(math.multiply(theta2, a2)));
+
+  return {
+    a1,
+    a2,
+    a3 
+  };
+};
+
+const costFunction = (theta1, theta2) => {
+  let X = [
+    [0, 1, 0, 1], 
+    [0, 0, 1, 1]
+  ];
+  let y = [
+    [0],
+    [1],
+    [1],
+    [0]
+  ];
+  let m = math.size(X)[1];
+  let n = math.size(X)[0];
+  let bigDelta1 = math.zeros(4, 3);
+  let bigDelta2 = math.zeros(1, 4);
+  let a3s = [
+    [0],
+    [0],
+    [0],
+    [0]
+  ];
+  
+  X = [[1, 1, 1, 1]].concat(X);
+
+
+  for (let i = 0; i < m; i++) {
+    let {a1, a2, a3} = forwardPropagate(math.subset(X, math.index(math.range(0, n + 1), i)), theta1, theta2);
+    a3s[i] = [a3];
+    let d3 = math.subtract(a3, math.squeeze(y[i]));
+    let d2 = math.dotMultiply(
+               math.dotMultiply(
+                 math.multiply(
+                   math.transpose(theta2), 
+                   d3
+                 ), 
+                 a2
+               ), 
+               math.subtract(1, a2)
+    );
+
+    bigDelta1 = math.add(bigDelta1, math.multiply(d2, math.transpose(a1)));
+    bigDelta2 = math.add(bigDelta2, math.multiply(d3, math.transpose(a2)));
+  }
+
+  let D1 = math.multiply(1/m, bigDelta1);
+  let D2 = math.multiply(1/m, bigDelta2);
+  let J = math.multiply(
+            -(1/m), 
+            math.add(
+              math.multiply(
+                math.transpose(y), 
+                a3s.map(row => row.map(val => math.log(val)))
+              ), 
+              math.multiply(
+                math.subtract(1, math.transpose(y)), 
+                math.subtract(1, a3s).map(row => row.map(val => math.log(val)))
+              )
+            )
+  );
+
+  return {
+    D1,
+    D2,
+    J
+  };
+};
+
+for (let i = 0; i < trainingCount; i++) {
+  let {D1, D2, J} = costFunction(theta1, theta2);
+
+  theta1 = math.subtract(theta1, math.multiply(alpha, D1)).toArray();
+  theta2 = math.subtract(theta2, math.multiply(alpha, D2)).toArray();
+}
+
+console.log(forwardPropagate([[1], [0], [0]], theta1, theta2).a3);
